2d875ee536111c5de054bc4ac087bbfffcecb1c8
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2022-01-05 21:58:47 +0100

    backend - router-tom v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import Login from './components/onboarding/Login';
 import Register from './components/onboarding/Register';
 
 function App() {
+
+  const token = localStorage.getItem('footballAccessToken');
+
   return (
     <div className="football-app">
-      <Home />
-      {/* <Register /> */}
+      <Routes>
+        <Route path="/" element={<Navigate to={!token ? '/register' : '/home'} />} />
+        <Route path="register" element={<Register />} /> :
+        <Route path="signin" element={<Login />} /> :
+        <Route path="home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/components/onboarding/Login.jsx
+++ src/components/onboarding/Login.jsx
@@ -1,28 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ClockLoader } from "react-spinners";
+import { useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap'
 
 
 function Login() {
+
+    const navigate = useNavigate()
+
+    const initialState = {
+        email: '',
+        password: '',
+    }
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [player, setPlayer] = useState(initialState);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const response = await fetch(`${process.env.REACT_APP_URL}/players/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(player)
+            })
+            if (response.ok) {
+                const data = await response.json();
+                localStorage.setItem('footballAccessToken', data.accessToken);
+                setLoading(false)
+                navigate('/home')
+            } else {
+                setError(true)
+                setLoading(false)
+            }
+
+        } catch (error) {
+            setError(true)
+            console.log(error);
+        }
+    }
+
+
     return (
         <div className='sign-in-form'>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className='form-group-item'>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={player.email}
+                        onChange={(e) => setPlayer({ ...player, email: e.target.value })}
+                    />
                 </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                <Form.Group className='form-group-item'>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        value={player.password}
+                        onChange={(e) => setPlayer({ ...player, password: e.target.value })}
+                    />
                 </Form.Group>
-                <Button
-                    className='form-button'
-                >
-                    Submit
-                </Button>
+                <div className='form-button-area'>
+                    {
+                        loading ?
+                            <ClockLoader color={"#fff"} size={25} />
+                            :
+                            <Button
+                                type="submit"
+                                className='form-button'
+                            >
+                                SIGN IN
+                            </Button>
+                    }
+                </div>
             </Form>
         </div>
     )
